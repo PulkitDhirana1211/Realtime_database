42525e170976968d22bc5a423716b56893d52927
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2022-01-12 14:36:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:realtime_database/pages/ui/front_page/front_page.dart';
 // ignore_for_file: prefer_const_constructors
+late final firstCamera;
 void main() async{
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
